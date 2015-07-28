70e790187df6467b32556d043a013465b868a3e5
A Jain <foreverrecompin@gmail.com>
2015-07-28 23:55:29 +0530

    Re: primeGenerator.py, second attempt at solving the question

--- primeGenerator.py
+++ primeGenerator.py
@@ -1,20 +1,37 @@
-def pg(m,n):
+import math
+primes = {2,3,5,7,11,13}
+
+def primechecker(number):
+    global primes
+    for i in range(3, int(math.sqrt(number)) + 1, 2):
+        if number % i  == 0:
+            break
+    else:
+        print(number)
+        primes.add(number)
+
+def pg_range(m,n):
+    global primes
     for num in range(m,n+1):
-        fact = 0
-        for i in range(1,num+1):
-            if num%i == 0:
-                fact +=1
-        if fact==2:
+        if num == 1 or (num%2 == 0 and num > 2):
+            continue
+        elif num == 2:
+            primes.add(2)
             print(num)
+            continue
+        elif num in primes:
+            print(num)
+            continue
+        elif primechecker(num):
+                continue
             
 def main():
-    x = []
-    testCases = int(input())
+    x, testCases = [], int(input())
     for i in range(0,testCases):
         x.append(list(map(int, input().split())))
     for each in x:
-        pg(each[0],each[1])
-        print()
+        pg_range(each[0],each[1])
+        print("\n")
         
 if __name__ == "__main__":
     main()
